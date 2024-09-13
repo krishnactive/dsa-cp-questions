6d72e07a88ed526b8ef8dbb9be73f1ab6307503d
krishna <krishnactive@gmail.com>
2024-09-13 19:06:40 +0530

    solved c

--- OneDrive/Desktop/C++COURSE/C_Smilo_and_Monsters.cpp
+++ OneDrive/Desktop/C++COURSE/C_Smilo_and_Monsters.cpp
@@ -1,50 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define long long ll
+#define ll long long
 
 int main(){
     int t;cin>>t;
     while(t--){
         int n;cin>>n;
-        vector<int> a(n);
-        for(int i=0;i<n;i++)cin>>a[i];
-        sort(begin(a),end(a));
-
-        int i=0, j= n-1;
-        int x = 0;
-        int attacks = 0;
-        while(i<j){
-            if(x<a[j]&&i!=j){
-                x+=a[i];
-                attacks+=a[i];
-                i++;
-            }
-            else if(x>=a[j]){
-                attacks++;
-                x = 0;
-                j--;
-            }
-            else if(i==j){
-                break; 
-            }
-        }
-        int ls = a[j];
-        if(x>=ls){
-            attacks++;
-        }
-        else if(x>=(ls+1)/2){
-            attacks+=1+(ls-x);
-        }
-        else{
-            for(int i=1;i<=(ls+1)/2;i++){
-                x++;
-                attacks++;
-                if(x>=ls-x){
-                    attacks++;
-                    break;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    multiset<ll> ms;
+    for(auto x:a)ms.insert(x);
+    ll ans=0;
+    ll x = 0;
+    while(!ms.empty()){
+        auto itb = ms.begin();
+        auto ite = ms.end();ite--;
+        if(itb!=ite){
+            if(x<*ite){
+                ll temp = min(*itb, *ite-x);
+                if(temp == *itb){
+                    x+=temp;
+                    ms.erase(itb);
+                }else{
+                    ll y = *itb -temp;
+                    x+=temp;
+                    ms.erase(itb);
+                    ms.insert(y);
                 }
+                ans+=temp;
+ 
+            }else{
+                ms.erase(ite);
+                ans++;
+                x=0;
             }
+        }else{
+            ll y = *itb;
+           
+            ans+= (y-x)/2;
+            ll temp = x;
+            x+= (y-temp)/2;
+            y-= (y-temp)/2;
+            if(x>0)ans++;
+            y-=x;
+            ans+=y;
+            // if(y-(y-x)/2 !=0)
+            break;
         }
-        cout<<attacks<<"\n";
+    }
+    cout<<ans<<endl;
     }
 }
